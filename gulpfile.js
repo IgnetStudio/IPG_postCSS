96799810d15491a2c26535518e019d478385a52e
Ignet <J.Ignatowicz@wit.edu.pl>
2017-06-28 16:13:13 +0200

    added stylelint, cssnano & assets

--- gulpfile.js
+++ gulpfile.js
@@ -3,12 +3,18 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var cssnext = require('cssnext');
 var precss = require('precss');
+// var stylelint = require('stylelint');
+// var cssnano = require('cssnano');
+var assets = require('postcss-assets');
 
 gulp.task('css', function () {
     var processors = [
         autoprefixer({browsers: ['last 5 versions']}),
         cssnext,
-        precss
+        precss,
+        // stylelint,
+        // cssnano,
+        assets
     ];
     return gulp.src('./src/*.css')
         .pipe(postcss(processors))
